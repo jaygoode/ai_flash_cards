c61c81b484df2950d3b7c090fefe52dcb3b51aca
johnny nylund <john_newcreek@protonmail.com>
2025-06-30 16:15:51 +0300

    restructure config  yaml

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import os
 
 if __name__ == "__main__":
     config = file_handler.read_yaml_file("config.yaml")
-    prompts = file_handler.read_yaml_file(config["prompts_fp"])
+    prompts = file_handler.read_yaml_file(config["filepaths"]["prompts_fp"])
 
-    if os.path.exists(config["anki_path"]) and not file_handler.is_anki_running():
-        subprocess.Popen([config["anki_path"]])
+    if os.path.exists(config["filepaths"]["anki_path"]) and not file_handler.is_anki_running():
+        subprocess.Popen([config["filepaths"]["anki_path"]])
         print("Anki launched!")
     else:
         print("Opening Anki failed. Might already be running.")
@@ -26,17 +26,17 @@ if __name__ == "__main__":
         card_amount = str(input("amount of cards to generate: "))
         text = input("further description for what the cards should be based on (or leave empty): ")
     else: 
-        topic = config["topic"]
-        use_file = config["use_file"]
-        deck_name = config["deck_name"]
-        card_amount = config["card_amount"]
-        text = config["text"]
+        topic = config["options"]["topic"]
+        use_file = config["options"]["use_file"]
+        deck_name = config["options"]["deck_name"]
+        card_amount = config["options"]["card_amount"]
+        text = config["options"]["text"]
 
 
 
     if use_file.lower() is any(["yes", "y"]):
         filename_key = input("yaml filename key value: ")
-        filepath = config[filename_key]
+        filepath = config["filepaths"][filename_key]
         text = "create cards based on this text: \n"
         text += file_handler.read_file(filepath)
         chunked_text = file_handler.chunk_text(text, model_name=config["model"])
@@ -44,8 +44,8 @@ if __name__ == "__main__":
     # cards_to_add = [
     # {"front": "Capital of France?", "back": "Paris", "tags": topic},
     # {"front": "2 + 2", "back": "4", "tags": topic},
-    # {"front": "Python creator?", "back": "Guido van Rossum", "tags": topic}
     # ]
+
     cards_total = [] #fix this
     for text_chunk in chunked_text:
         filled_prompt = prompts["generate_flashcards"].replace("{{topic}}", topic).replace("{{text}}", text).replace("{{card_amount}}", card_amount)
