9747e7625b13003d74a7b79281d925ed56297a37
johnny <john_newcreek@protonmail.com>
2025-08-15 16:20:16 +0300

    refactor main

--- main.py
+++ main.py
@@ -7,15 +7,42 @@ from typing import Dict
 import platform 
 import glob 
 
+def create_from_deck_json(config, os_name):
+    print(
+        f"Using readymade deck: {config['options']['readymade_deck_name']}. No new cards will be created."
+    )
+    breakpoint()
+    cards = file_handler.read_json_file(
+        config["filepaths"][os_name]["decks_path"] + config["options"]["readymade_deck_name"] + ".json"
+    )
+    if not cards:
+        print("No cards found in the readymade deck file.")
+        exit(1)
+    print(f"Cards found: {len(cards)}")
+    print(f"Adding cards to deck: {config['options']['readymade_deck_name']}")
+    anki_handler.add_cards(config['options']["readymade_deck_name"], cards)
+
+def create_deck_with_ai(config):
+    options: Dict[str, str] = helpers.get_settings(config["options"]["use_inputs"], config)
+    filename:str = ""
+    for chunk in file_handler.chunk_text(options["topic"]):
+        filename = helpers.generate_cards(options, config, prompts, chunk)
+    cards = file_handler.read_json_file(filename)
+    anki_handler.add_cards(options["deck_name"], cards)
+
+    print(
+        f'''card creation done! deck name: {options["deck_name"]}, topic: {options["topic"]}, cards created: {len(cards)}'''
+    )
+
 if __name__ == "__main__":
     os_name = platform.system().lower()
     config = file_handler.read_yaml_file("config.yaml")
     prompts = file_handler.read_yaml_file(config["filepaths"][os_name]["prompts_fp"])
-    # installation_folder = config["filepaths"][os_name]["anki_path"]
-    # installation_file = glob.glob(os.path.join(installation_folder, config["filepaths"][os_name]["installer_name"]))[0]
+    installation_folder = config["filepaths"][os_name]["anki_path"]
+    installation_file = glob.glob(os.path.join(installation_folder, config["filepaths"][os_name]["installer_name"]))[0]
 
-    # if not os.path.exists(config["filepaths"][os_name]["anki_exe_path"]):
-    #     anki_handler.download_anki_installation_file(dest_folder=config["filepaths"][os_name]["anki_path"])
+    if not os.path.exists(config["filepaths"][os_name]["anki_exe_path"]):
+        anki_handler.download_anki_installation_file(dest_folder=config["filepaths"][os_name]["anki_path"])
 
     if not file_handler.is_anki_running():
         subprocess.Popen([config["filepaths"][os_name]["anki_exe_path"]])
@@ -24,27 +51,6 @@ if __name__ == "__main__":
         print("Opening Anki failed. Might already be running.")
 
     if config["options"]["use_readymade_deck"].lower() in ["yes", "y"]:
-        print(
-            f"Using readymade deck: {config['options']['readymade_deck_name']}. No new cards will be created."
-        )
-        breakpoint()
-        cards = file_handler.read_json_file(
-            config["filepaths"][os_name]["decks_path"] + config["options"]["readymade_deck_name"] + ".json"
-        )
-        if not cards:
-            print("No cards found in the readymade deck file.")
-            exit(1)
-        print(f"Cards found: {len(cards)}")
-        print(f"Adding cards to deck: {config['options']['readymade_deck_name']}")
-        anki_handler.add_cards(config['options']["readymade_deck_name"], cards)
+        create_from_deck_json(config, os_name)
     else:    
-        options: Dict[str, str] = helpers.get_settings(config["options"]["use_inputs"], config)
-        filename:str = ""
-        for chunk in file_handler.chunk_text(options["topic"]):
-            filename = helpers.generate_cards(options, config, prompts, chunk)
-        cards = file_handler.read_json_file(filename)
-        anki_handler.add_cards(options["deck_name"], cards)
-
-        print(
-            f'''card creation done! deck name: {options["deck_name"]}, topic: {options["topic"]}, cards created: {len(cards)}'''
-        )
+        create_from_deck_json(config, os_name)
